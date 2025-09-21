1c7b5cec35aeeb5d5f1efd8e5f9f61442a96940e
jsofiah <j.sofiah18@gmail.com>
2025-09-21 22:24:50 +0700

    Praktikum 3 No 3

---
+++ jobsheet_4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil dari penjumlahan $a dan $b adalah $hasilTambah <br><br>";
+echo "Hasil dari pengurangan $a dan $b adalah $hasilKurang <br><br>";
+echo "Hasil dari perkalian $a dan $b adalah $hasilKali <br><br>";
+echo "Hasil dari pembagian $a dan $b adalah $hasilBagi <br><br>";
+echo "Hasil dari sisa bagi $a dan $b adalah $sisaBagi <br><br>";
+echo "Hasil dari $a pangkat $b adalah $pangkat <br><br>";
+?>
