01caa022b7a57046a631c4d0be7daa4d9022c62f
jsofiah <j.sofiah18@gmail.com>
2025-09-22 10:21:29 +0700

    Praktikum 4 no 12

--- jobsheet_4/struktur_kontrol.php
+++ jobsheet_4/struktur_kontrol.php
@@ -21,5 +21,16 @@ while ($jarakSaatIni < $jarakTarget){
     $hari++;
 }
 
-echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer";
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer <br><br>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 ?>
