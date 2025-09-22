df48ee4ce7e0d6f216c082d597bb7afa360a0d2f
jsofiah <j.sofiah18@gmail.com>
2025-09-22 12:46:38 +0700

    Praktikum 4 no  20

--- jobsheet_4/struktur_kontrol.php
+++ jobsheet_4/struktur_kontrol.php
@@ -41,5 +41,14 @@ foreach ($skorUjian as $skor){
     $totalSkor += $skor;
 }
 
-echo "Total skor ujian adalah: $totalSkor"
+echo "Total skor ujian adalah: $totalSkor <br><br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+foreach ($nilaiSiswa as $nilai){
+    if ($nilai < 60){
+        echo "Nilai $nilai (Tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (lulus) <br>";
+}
 ?>
