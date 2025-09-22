c1c830063ee01c6975d833489d8e24a768d4401f
jsofiah <j.sofiah18@gmail.com>
2025-09-22 09:53:49 +0700

    Praktikum 4 no 8

--- jobsheet_4/struktur_kontrol.php
+++ jobsheet_4/struktur_kontrol.php
@@ -2,12 +2,24 @@
 $nilaiNumerik = 92;
 
 if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100){
-    echo "Nilai huruf: A";
+    echo "Nilai huruf: A <br><br>";
 } elseif ($nilaiNumerik >= 80 && $nilaiNumerik <= 90){
-    echo "Nilai huruf: B";
+    echo "Nilai huruf: B <br><br>";
 } elseif ($nilaiNumerik >= 70 && $nilaiNumerik <= 80){
-    echo "Nilai huruf: C";
+    echo "Nilai huruf: C <br><br>";
 } elseif ($nilaiNumerik < 70){
-    echo "Nilai huruf: D";
+    echo "Nilai huruf: D <br><br>";
 }
+
+$jarakSaatIni = 0;
+$jarakTarget = 500;
+$peningkatanHarian = 30;
+$hari = 0;
+
+while ($jarakSaatIni < $jarakTarget){
+    $jarakSaatIni += $peningkatanHarian;
+    $hari++;
+}
+
+echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer";
 ?>
