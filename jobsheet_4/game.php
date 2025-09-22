1a61d5a55dfb685957249b1d15d5ff0bd2d0c5e9
jsofiah <j.sofiah18@gmail.com>
2025-09-22 20:55:47 +0700

    Praktikum 4 no 26

---
+++ jobsheet_4/game.php
@@ -0,0 +1,11 @@
+<?php
+$poin = 510;
+$batas = 500;
+
+echo "Total skor pemain adalah: ($poin) <br>";
+if($poin > $batas) {
+    echo "Apakah pemain mendapatkan hadiah tambahan? (YA)";
+} else {
+    echo "Apakah pemain mendapatkan hadiah tambahan? (TIDAK)";
+}
+?>
