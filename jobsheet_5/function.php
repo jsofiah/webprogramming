11d935d2667760e6239346f13364757c241944a8
jsofiah <j.sofiah18@gmail.com>
2025-09-27 19:20:23 +0700

    Praktikum 4 no 4

--- jobsheet_5/function.php
+++ jobsheet_5/function.php
@@ -1,10 +1,16 @@
 <?php
-    function perkenalan(){
-        echo "Assalamualaikum, ";
-        echo "Perkenalkan, nama saya Sofiah<br/>";
+    function perkenalan($nama, $salam){
+        echo $salam . ", ";
+        echo "Perkenalkan, nama saya " . $nama ."<br/>";
         echo "Senang berkenalan dengan Anda<br/>";
     }
 
-    perkenalan();
-    perkenalan();
+    perkenalan("Sofiah", "Hallo");
+
+    echo "<hr>";
+
+    $saya = "Sofi";
+    $ucapanSalam = "Selamat pagi";
+
+    perkenalan($saya, $ucapanSalam);
 ?>
