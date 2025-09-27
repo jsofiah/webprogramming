89a48781b268b0e4634ba66b79117d9a35edae35
jsofiah <j.sofiah18@gmail.com>
2025-09-27 14:39:14 +0700

    Praktikum 1 no 4

--- jobsheet_5/array_1.php
+++ jobsheet_5/array_1.php
@@ -7,9 +7,13 @@
     <h2>Array Terindeks</h2>
     <?php
     $ListDosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-    echo $ListDosen[2] . "<br>";
-    echo $ListDosen[0] . "<br>";
-    echo $ListDosen[1] . "<br>";
+    // echo $ListDosen[2] . "<br>";
+    // echo $ListDosen[0] . "<br>";
+    // echo $ListDosen[1] . "<br>";
+
+    foreach ($ListDosen as $Dosen) {
+        echo $Dosen . "<br>";
+    }
     ?>
 </body>
 </html>
