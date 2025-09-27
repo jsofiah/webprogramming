e18f1c792a19bf998bc3086e9b4e11dca23d2b86
jsofiah <j.sofiah18@gmail.com>
2025-09-27 19:43:33 +0700

    Praktikum 4 no 13

--- jobsheet_5/function_return.php
+++ jobsheet_5/function_return.php
@@ -3,6 +3,13 @@
         $umur = $thn_sekarang - $thn_lahir;
         return $umur;
     }
+    function perkenalan($nama, $salam="Assalamualaikum"){
+        echo $salam . ", ";
+        echo "Perkenalkan, nama saya " . $nama ."<br/>";
 
-    echo "Umur saya adalah " . hitungUmur(2006, 2025) . "tahun";
+        echo "Saya berusia " . hitungUmur(2006, 2025) . " tahun<br/>";
+        echo "Senang berkenalan dengan anda <br/>";
+    }
+
+    perkenalan("Sofi");
 ?>
