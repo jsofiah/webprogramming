ceb0e8b2825e0bd995528fe06d05aaeff4977bc8
jsofiah <j.sofiah18@gmail.com>
2025-09-27 20:39:25 +0700

    Praktikum 6 no 5

--- jobsheet_5/menu.php
+++ jobsheet_5/menu.php
@@ -37,6 +37,9 @@
         echo "<ul>";
         foreach ($menu as $key => $item) {
             echo "<li>{$item['nama']}</li>";
+            if(isset($item["subMenu"])){
+                tampilkanAngka($item["subMenu"]);
+            }
         }
         echo "</ul>";
     }
