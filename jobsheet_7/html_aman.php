77588821d8b7298a3b967615a682b64d299f9217
jsofiah <j.sofiah18@gmail.com>
2025-10-11 01:08:42 +0700

    Praktikum 4 no 6 & 7

--- jobsheet_7/html_aman.php
+++ jobsheet_7/html_aman.php
@@ -8,14 +8,23 @@
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <label for="input">Masukkan text:</label>
         <input type="text" name="input" id="input"><br><br>
+        <label for="email">Masukkan email:</label>
+        <input type="email" name="email" id="email"><br><br>
 
         <input type="submit" name="submit" value="Submit"><br><br>
     </form>
     <?php
         if($_SERVER["REQUEST_METHOD"] == "POST"){
-            $input = $_POST["input"];
+            $input = $_POST['input'];
             $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-            echo "Hasil input: " . $input;
+            echo "Hasil input: " . $input . "<br>";
+
+            $email = $_POST['email'];
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo "Email anda valid dan telah diproses dengan aman<br>";
+            } else {
+                echo "Alamat email tidak valid! Silakan masukkan format email yang benar<br>";
+            }
         }
     ?>
 </body>
