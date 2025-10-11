d98e354ee83d146f894bf13724ffe819f619d242
jsofiah <j.sofiah18@gmail.com>
2025-10-11 14:36:44 +0700

    Praktikum 5 no 14

--- jobsheet_7/regex.php
+++ jobsheet_7/regex.php
@@ -16,9 +16,17 @@
     //     echo "Tidak ada yang cocok!";
     // }
 
-    $pattern = '/apple/';
-    $replacement = 'banana';
-    $text = 'I like apple pie.';
-    $new_text = preg_replace($pattern, $replacement, $text);
-    echo $new_text;
+    // $pattern = '/apple/';
+    // $replacement = 'banana';
+    // $text = 'I like apple pie.';
+    // $new_text = preg_replace($pattern, $replacement, $text);
+    // echo $new_text;
+
+    $pattern = '/go*d/';
+    $text = 'god is good.';
+    if(preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
 ?>
