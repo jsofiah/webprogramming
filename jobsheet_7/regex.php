c2c3a5481e4d1e4233b4fc4fc97ccf2bb68d4946
jsofiah <j.sofiah18@gmail.com>
2025-10-11 14:22:21 +0700

    Praktikum 5 no 10

--- jobsheet_7/regex.php
+++ jobsheet_7/regex.php
@@ -7,12 +7,18 @@
     // } else {
     //     echo "Tidak ada huruf kecil!";
     // }
-    $pattern = '/[0-9]+/';
-    $text = 'There are 123 apples.';
+    // $pattern = '/[0-9]+/';
+    // $text = 'There are 123 apples.';
 
-    if(preg_match( $pattern, $text, $matches ) ) {
-        echo "Cocokkan: " . $matches[0];
-    } else {
-        echo "Tidak ada yang cocok!";
-    }
+    // if(preg_match( $pattern, $text, $matches ) ) {
+    //     echo "Cocokkan: " . $matches[0];
+    // } else {
+    //     echo "Tidak ada yang cocok!";
+    // }
+
+    $pattern = '/apple/';
+    $replacement = 'banana';
+    $text = 'I like apple pie.';
+    $new_text = preg_replace($pattern, $replacement, $text);
+    echo $new_text;
 ?>
