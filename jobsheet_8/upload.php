5033e752e6bdab02077570657ac1e7607da4840b
jsofiah <j.sofiah18@gmail.com>
2025-10-22 00:04:46 +0700

    Praktikum 1 no 10

--- jobsheet_8/upload.php
+++ jobsheet_8/upload.php
@@ -4,15 +4,12 @@
         $targetfile = $targetdir . basename($_FILES["myFile"]["name"]);
         $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-        $maxsize = 5*1024*1024;
+        $allowedExtensions = array("txt", "pdf", "doc", "docx");
+        $maxsize = 3*1024*1024;
 
         if(in_array($fileType, $allowedExtensions) && $_FILES["myFile"]["size"]<=$maxsize){
             if(move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)){
                 echo "File berhasil diunggah.";
-
-                echo "<p>Preview Gambar:</p>";
-                echo "<img src='$targetfile' width='200' style='height:auto; border:1px solid #ccc; border-radius:8px;'>";
             } else {
                 echo "Gagal mengunggal file.";
             }
