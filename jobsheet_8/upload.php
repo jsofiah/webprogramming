7ad688a00c9512ae990dadc71a7c861d999a910e
jsofiah <j.sofiah18@gmail.com>
2025-10-21 22:54:32 +0700

    Praktikum 1 no 6

--- jobsheet_8/upload.php
+++ jobsheet_8/upload.php
@@ -2,11 +2,20 @@
     if(isset($_POST["submit"])){
         $targetdir = "uploads/";
         $targetfile = $targetdir . basename($_FILES["myFile"]["name"]);
+        $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-        if(move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)){
-            echo "File berhasil diunggah.";
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxsize = 5*1024*1024;
+
+        if(in_array($fileType, $allowedExtensions) && $_FILES["myFile"]["size"]<=$maxsize){
+            if(move_uploaded_file($_FILES["myFile"]["tmp_name"], $targetfile)){
+                echo "File berhasil diunggah.";
+            } else {
+                echo "Gagal mengunggal file.";
+            }
         } else {
-            echo "Gagal mengunggal file.";
+            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
         }
+
     }
 ?>
