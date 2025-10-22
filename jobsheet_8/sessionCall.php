64d321163d90ec2a23be38a10d052bdafd5f606f
jsofiah <j.sofiah18@gmail.com>
2025-10-22 14:41:51 +0700

    Praktikum 8

---
+++ jobsheet_8/sessionCall.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+        </head>
+        <body>
+            <?php
+                echo "Favorite color is " . $_SESSION["favcolor"] . "<br>";
+                echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+            ?>
+        </body>
+</html>
