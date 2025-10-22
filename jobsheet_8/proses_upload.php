cc0970801845b1c427048c3023c429f457cb4b47
jsofiah <j.sofiah18@gmail.com>
2025-10-22 08:45:50 +0700

    Praktikum 2 no 5

--- jobsheet_8/proses_upload.php
+++ jobsheet_8/proses_upload.php
@@ -1,19 +1,24 @@
 <?php
-    $targetDirectory = "documents/";
+    $targetDirectory = "images/";
 
     if(!file_exists($targetDirectory)){
         mkdir($targetDirectory, 0777, true);
     }
 
-    if($_FILES['files']['name'][0]){
-        $totalFiles = count($_FILES['files']['name']);
+    if(isset($_FILES['images']['name'][0])){
+        $totalFiles = count($_FILES['images']['name']);
 
-        for ($i = 0; $i < $totalFiles; $i++){
-            $fileName = $_FILES['files']['name'][$i];
-            $targetFile = $targetDirectory . $fileName;
+        $allowedExtensions = array("jpg", "jpeg", "png", "gif");
 
-            if (move_uploaded_file($_FILES['files']['tmp_name'][$i],$targetFile)){
-                echo "File $fileName berhasil diunggah. <br>";
+        for($i = 0; $i < $totalFiles; $i++){
+            $fileName = $_FILES['images']['name'][$i];
+            $targetFile = $targetDirectory . basename($fileName);
+            $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+            if(move_uploaded_file($_FILES['images']['tmp_name'][$i], $targetFile)){
+                echo "File $fileName berhasil diunggah.<br>";
+
+                echo "<img src='$targetFile' width='200' style='height:auto; margin:10px; border:1px solid #ccc; border-radius:8px;'><br>";
             } else {
                 echo "Gagal mengunggah file $fileName.<br>";
             }
